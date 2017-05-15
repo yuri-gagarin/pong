555464330da12793824c387be11790cd174f190e
Yuriy Ivanov <ymivanov@icloud.com>
2017-05-14 22:19:12 -0400

    added a static ball and canvas

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const canvas  = document.getElementById("pong");
+const context = canvas.getContext('2d');
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+context.fillStyle = "#fff";
+context.fillRect(20, 20, 10, 10);
